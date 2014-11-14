7049d61baefc676712d9ad1db0a184f68b38664a
Thomas Rausch <thomas@rauschig.org>
2014-11-14 20:56:23 +0100

    Add more dialogs examples

--- flex-ui-examples/src/main/java/org/cdlflex/ui/pages/examples/DialogsPage.java
+++ flex-ui-examples/src/main/java/org/cdlflex/ui/pages/examples/DialogsPage.java
@@ -13,15 +13,28 @@
  */
 package org.cdlflex.ui.pages.examples;
 
-import org.apache.wicket.markup.html.link.AbstractLink;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.cdlflex.ui.behavior.ButtonBehavior;
 import org.cdlflex.ui.markup.css.Buttons;
+import org.cdlflex.ui.markup.html.basic.DateLabel;
 import org.cdlflex.ui.markup.html.dialog.Dialog;
 import org.cdlflex.ui.markup.html.dialog.DialogOpenLink;
+import org.cdlflex.ui.markup.html.dialog.form.FormDialog;
+import org.cdlflex.ui.markup.html.form.DateTimePicker;
 import org.cdlflex.ui.model.Person;
 import org.cdlflex.ui.pages.ExamplePage;
+import org.rauschig.wicketjs.ajax.JsAjaxLink;
 
 /**
  * DialogsPage.
@@ -29,36 +42,85 @@ import org.cdlflex.ui.pages.ExamplePage;
 public class DialogsPage extends ExamplePage {
     private static final long serialVersionUID = 1L;
 
+    private IModel<Person> person = new Model<>(new Person(1, "Alan Turing", "1912-06-23"));
+
     public DialogsPage() {
-        Dialog<Person> dialog = new PersonDialog("dialog");
+        Dialog<Person> personDetailDialog = newPersonDetailDialog("person-detail-dialog");
+        Dialog<Person> personEditDialog = newPersonEditDialog("person-edit-dialog");
 
-        dialog.addButton(new AbstractLink(Dialog.BUTTON_MARKUP_ID) {
-            private static final long serialVersionUID = 1L;
+        add(personDetailDialog);
+        add(personEditDialog);
 
-            @Override
-            protected void onInitialize() {
-                super.onInitialize();
-                add(new ButtonBehavior(Buttons.Type.PRIMARY));
-            }
-        }.setBody(Model.of("Save changes")));
-
-        dialog.addCloseButton(Model.of("Close"));
-
-        dialog.setTitle(Model.of("Person"));
-
-        add(dialog);
-        add(new DialogOpenLink("open-button", dialog));
+        add(new DialogOpenLink("open-person-detail-dialog", personDetailDialog));
+        add(new DialogOpenLink("open-person-edit-dialog", personEditDialog));
     }
 
-    private class PersonDialog extends Dialog<Person> {
+    private Dialog<Person> newPersonDetailDialog(String id) {
+        Dialog<Person> dialog = new PersonDetailDialog(id, person);
+
+        dialog.addCloseButton(Model.of("Close"));
+        dialog.setTitle(Model.of("Person"));
+
+        return dialog;
+    }
+
+    private FormDialog<Person> newPersonEditDialog(String id) {
+        return new PersonEditDialog(id, person);
+    }
+
+    private class PersonEditDialog extends FormDialog<Person> {
         private static final long serialVersionUID = 1L;
 
-        private PersonDialog(String id) {
+        private PersonEditDialog(String markupId, IModel<Person> model) {
+            super(markupId, Model.of("Edit"), model);
+        }
+
+        @Override
+        protected Form<Person> newForm(IModel<Person> model) {
+            Form<Person> form = new Form<>("form", new CompoundPropertyModel<>(model));
+            form.add(new TextField<>("name", String.class));
+            form.add(new DateTimePicker("birthday", "y-MM-dd").setPickTime(false));
+            return form;
+        }
+    }
+
+    private class PersonDetailDialog extends Dialog<Person> {
+        private static final long serialVersionUID = 1L;
+
+        private PersonDetailDialog(String id) {
             super(id);
         }
 
-        private PersonDialog(String id, IModel<Person> model) {
+        private PersonDetailDialog(String id, IModel<Person> model) {
             super(id, model);
+        }
+
+        @Override
+        protected void onInitialize() {
+            super.onInitialize();
+
+            add(new Label("id", new PropertyModel<>(getModel(), "id")).setVisible(false)
+                    .setOutputMarkupPlaceholderTag(true).setOutputMarkupId(true));
+            add(new Label("name", new PropertyModel<>(getModel(), "name")));
+            add(new DateLabel("birthday", new SimpleDateFormat("y-MM-dd"), new PropertyModel<Date>(getModel(),
+                    "birthday")));
+
+            prependButton(new JsAjaxLink<Void>(Dialog.BUTTON_MARKUP_ID) {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                protected void onInitialize() {
+                    super.onInitialize();
+                    add(new ButtonBehavior(Buttons.Type.INFO));
+                }
+
+                @Override
+                public void onClick(AjaxRequestTarget target) {
+                    Component container = PersonDetailDialog.this.get("id");
+                    container.setVisible(true);
+                    target.add(container);
+                }
+            }.setBody(Model.of("Show ID")));
         }
     }
 }
