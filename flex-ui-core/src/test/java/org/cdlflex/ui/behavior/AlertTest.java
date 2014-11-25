e81060e6c94c4706f2cfcebcacb0ef72dc1c0ad2
Thomas Rausch <thomas@rauschig.org>
2014-11-25 22:46:37 +0100

    Fix deprecated junit import

--- flex-ui-core/src/test/java/org/cdlflex/ui/behavior/AlertTest.java
+++ flex-ui-core/src/test/java/org/cdlflex/ui/behavior/AlertTest.java
@@ -13,7 +13,7 @@
  */
 package org.cdlflex.ui.behavior;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
